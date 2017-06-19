4b5f57935bea6edc8d2e0d042d967f2f6d598c21
aditya1812 <adityamore950@gmail.com>
2017-06-19 12:47:27 -0500

    Add files via upload

---
+++ Anagrams/src/Anagrams.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 5CB4125SDX0 on 18-06-2017.
+ */
+public class Anagrams {
+    public static void main(String[] args) {
+        String s1 = "ilisatien";
+        String s2 = "lpisitein";
+        boolean anagram = true;
+        Map<Character, Integer> m = new HashMap<>();
+        if(!(s1.length() == s2.length())) {
+            System.out.println("Length not equal.");
+            anagram = false;
+        }
+
+        for(int i = 0; i < s1.length(); i++) {
+            if (m.containsKey(s1.charAt(i)))
+                m.put(s1.charAt(i), m.get(s1.charAt(i)) + 1);
+            else
+                m.put(s1.charAt(i), 1);
+        }
+
+        for(int i = 0; i < s2.length(); i++) {
+            if(!m.containsKey(s2.charAt(i))) {
+                anagram = false;
+                break;
+            }
+            else
+            {
+                m.put(s1.charAt(i), m.get(s1.charAt(i)) - 1);
+            }
+        }
+
+        if(anagram)
+            System.out.println("Anagrams");
+        else
+            System.out.println("Not Anagrams");
+
+        List<ArrayList<Integer>> ans = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
+        a.
+
+    }
+}
